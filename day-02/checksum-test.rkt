770d22be3716e8f8e3ee92e57bf8e15efdf6cbc6
Gabriel Birke <gabriel.birke@wikimedia.de>
2019-11-28 17:08:57 +0100

    Add char hash function

--- day-02/checksum-test.rkt
+++ day-02/checksum-test.rkt
@@ -3,6 +3,11 @@
 (require rackunit
          "checksum.rkt")
 
-(check-equal? (count-chars (char-count 0 0) "") (char-count 0 0) "Empty string should not increase count")
-(check-equal? (count-chars (char-count 0 0) "ab") (char-count 0 0) "non duplicate chars should not increase count")
-(check-equal? (count-chars (char-count 0 0) "aa") (char-count 1 0) "double chars should increase double count by 1")
+(check-equal? (checksum-parts (char-count 0 0) "") (char-count 0 0) "Empty string should not increase count")
+(check-equal? (checksum-parts (char-count 0 0) "ab") (char-count 0 0) "non duplicate chars should not increase count")
+; (check-equal? (checksum-parts (char-count 0 0) "aa") (char-count 1 0) "double chars should increase double count by 1")
+
+;(check-equal (count-chars "abcd") #hash(("a" . 1) ("b" . 1) ("c" . 1) ("d" . 1)) "should count characters")
+
+(check-equal? (next-char-hash "a" #hash()) #hash(("a" . 1)) "Should create a hash")
+(check-equal? (next-char-hash "a" #hash(("a" . 1)("b" . 1))) #hash(("a" . 2)("b" . 1)) "Should create an updated hash")
--- day-02/checksum.rkt
+++ day-02/checksum.rkt
@@ -2,11 +2,18 @@
 
 (struct char-count (two three) #:transparent )
 
-(define (count-chars current-count input-str)
+(define (checksum-parts current-count input-str)
   current-count
   )
 
-(provide char-count
-         count-chars
+(define (count-chars str) #f)
 
+(define (next-char-hash current-char result-hash)
+  (hash-set result-hash current-char (+ 1 (hash-ref result-hash current-char 0)))
+  )
+  
+(provide char-count
+         checksum-parts
+         count-chars
+         next-char-hash
          )
